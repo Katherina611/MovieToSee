7078877e7588a5b0d0555996748ddb0213a15857
Katherina611 <katarzynag0609@gmail.com>
2018-02-21 15:33:11 +0100

    added ratings

--- js/app.jsx
+++ js/app.jsx
@@ -20,13 +20,27 @@ function addUser(){
         $newSpan.text(movieVal);
         $newDecription.text(descVAl);
 
+        //rozwiazanie z gwiazdkami//
+        let $stars=$('<div class="stars"></div>');
+        let $newStar1=$('<span class="star1">★</span>');
+        let $newStar2=$('<span class="star2">★</span>');
+        let $newStar3=$('<span class="star3">★</span>');
+        let $newStar4=$('<span class="star4">★</span>');
+        let $newStar5=$('<span class="star5">★</span>');
+        $stars.append($newStar1);
+        $stars.append($newStar2);
+        $stars.append($newStar3);
+        $stars.append($newStar4);
+        $stars.append($newStar5);
+
 
         $section.find('ul').append($newLi);
         $newLi.append($newSpan);
         $newLi.append($newDecription);
         $newLi.append($newImage);
-        $newLi.append(newBtn);
-        $newLi.append(editBtn);
+        $newLi.append($stars);
+        $newSpan.append(newBtn);
+        $newSpan.append(editBtn);
     })
 
 }
@@ -45,6 +59,51 @@ $('body').on('click', '.accept', function () {
     $(this).attr("class", "button2");
     $(this).text("Edit");
 
+});
+$('body').on('click', '.star1', function () {
+    if($(this).hasClass('star1')){
+        $(this).css('color',"orange");
+        $(this).next().css('color',"grey");
+        $(this).next().next().css('color',"grey");
+        $(this).next().next().next().css('color',"grey");
+        $(this).next().next().next().next().css('color',"grey");
+    }
+});
+$('body').on('click', '.star2', function () {
+    if($(this).hasClass('star2')){
+        $(this).css('color',"orange");
+        $(this).prev().css('color',"orange");
+        $(this).next().css('color',"grey");
+        $(this).next().next().css('color',"grey");
+        $(this).next().next().next().css('color',"grey");
+    }
+});
+$('body').on('click', '.star3', function () {
+    if($(this).hasClass('star3')){
+        $(this).css('color',"orange");
+        $(this).prev().css('color',"orange");
+        $(this).prev().prev().css('color',"orange");
+        $(this).next().css('color',"grey");
+        $(this).next().next().css('color',"grey");
+    }
+});
+$('body').on('click', '.star4', function () {
+    if($(this).hasClass('star4')){
+        $(this).css('color',"orange");
+        $(this).prev().css('color',"orange");
+        $(this).prev().prev().css('color',"orange");
+        $(this).prev().prev().prev().css('color',"orange");
+        $(this).next().css('color',"grey");
+    }
+});
+$('body').on('click', '.star5', function () {
+    if($(this).hasClass('star5')){
+        $(this).css('color',"orange");
+        $(this).prev().css('color',"orange");
+        $(this).prev().prev().css('color',"orange");
+        $(this).prev().prev().prev().css('color',"orange");
+        $(this).prev().prev().prev().prev().css('color',"orange");
+    }
 });
 
 addUser();
--- js/out.js
+++ js/out.js
@@ -76,11 +76,6 @@ function addUser() {
     var $movie = $section.find('#addMovie');
     var $description = $section.find('#addDesc');
     var $images = $section.find('#addImg');
-    var $li = $section.find('li').find('span');
-    var newBtn = $('<button class="button1">Oglądnąłem</button>');
-    var editBtn = $('<button class="button2">Edytuj tytuł</button>');
-    $li.append(newBtn);
-    $li.append(editBtn);
 
     btn.on('click', function () {
         var movieVal = $movie.val();
@@ -97,10 +92,24 @@ function addUser() {
         $newSpan.text(movieVal);
         $newDecription.text(descVAl);
 
+        //rozwiazanie z gwiazdkami//
+        var $stars = $('<div class="stars"></div>');
+        var $newStar1 = $('<span class="star1">★</span>');
+        var $newStar2 = $('<span class="star2">★</span>');
+        var $newStar3 = $('<span class="star3">★</span>');
+        var $newStar4 = $('<span class="star4">★</span>');
+        var $newStar5 = $('<span class="star5">★</span>');
+        $stars.append($newStar1);
+        $stars.append($newStar2);
+        $stars.append($newStar3);
+        $stars.append($newStar4);
+        $stars.append($newStar5);
+
         $section.find('ul').append($newLi);
         $newLi.append($newSpan);
         $newLi.append($newDecription);
         $newLi.append($newImage);
+        $newLi.append($stars);
         $newSpan.append(newBtn);
         $newSpan.append(editBtn);
     });
@@ -118,6 +127,51 @@ $('body').on('click', '.accept', function () {
     $(this).parent().attr("contenteditable", "false");
     $(this).attr("class", "button2");
     $(this).text("Edit");
+});
+$('body').on('click', '.star1', function () {
+    if ($(this).hasClass('star1')) {
+        $(this).css('color', "orange");
+        $(this).next().css('color', "grey");
+        $(this).next().next().css('color', "grey");
+        $(this).next().next().next().css('color', "grey");
+        $(this).next().next().next().next().css('color', "grey");
+    }
+});
+$('body').on('click', '.star2', function () {
+    if ($(this).hasClass('star2')) {
+        $(this).css('color', "orange");
+        $(this).prev().css('color', "orange");
+        $(this).next().css('color', "grey");
+        $(this).next().next().css('color', "grey");
+        $(this).next().next().next().css('color', "grey");
+    }
+});
+$('body').on('click', '.star3', function () {
+    if ($(this).hasClass('star3')) {
+        $(this).css('color', "orange");
+        $(this).prev().css('color', "orange");
+        $(this).prev().prev().css('color', "orange");
+        $(this).next().css('color', "grey");
+        $(this).next().next().css('color', "grey");
+    }
+});
+$('body').on('click', '.star4', function () {
+    if ($(this).hasClass('star4')) {
+        $(this).css('color', "orange");
+        $(this).prev().css('color', "orange");
+        $(this).prev().prev().css('color', "orange");
+        $(this).prev().prev().prev().css('color', "orange");
+        $(this).next().css('color', "grey");
+    }
+});
+$('body').on('click', '.star5', function () {
+    if ($(this).hasClass('star5')) {
+        $(this).css('color', "orange");
+        $(this).prev().css('color', "orange");
+        $(this).prev().prev().css('color', "orange");
+        $(this).prev().prev().prev().css('color', "orange");
+        $(this).prev().prev().prev().prev().css('color', "orange");
+    }
 });
 
 addUser();
